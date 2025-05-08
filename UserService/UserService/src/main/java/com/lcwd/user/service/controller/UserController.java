7c0269f92d3ad082ca9381f7b5d838b66c71ac75
vikas054 <vikaschauhan054@gmail.com>
2025-05-08 12:12:11 +0530

    Added Configuration server

--- UserService/UserService/src/main/java/com/lcwd/user/service/controller/UserController.java
+++ UserService/UserService/src/main/java/com/lcwd/user/service/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.lcwd.user.service.controller;
 import com.lcwd.user.service.entity.User;
 import com.lcwd.user.service.services.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +18,8 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
+
     //create
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user){
@@ -27,10 +32,23 @@ public class UserController {
     //single user get
 
     @GetMapping("/{user_id}")
+    @CircuitBreaker(name="ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getSingleUser(@PathVariable("user_id") String userId){// if both userid are same then no need to add name in pathvariable
 
         User user = userService.getUser(userId);
         return ResponseEntity.ok(user);
+    }
+
+    //creating fallbackmethod for circuit breaker
+    public ResponseEntity<User> ratingHotelFallback(String userId, Exception exception){
+        logger.info("Fallback is executed, because service is down : "+exception.getMessage());
+        User user = User.builder()
+                    .email("dummy@gmail.com")
+                    .name("Dummy")
+                    .about("This use is created dummy because some services is down")
+                    .userId("2134")
+                    .build();
+        return new ResponseEntity<>(user,HttpStatus.OK);
     }
 
     // get all user
